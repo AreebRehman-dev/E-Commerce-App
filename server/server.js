7957061dfd19fa69be38fb3f1703905c071215ff
Areeb Rehman <areebrehman411@gmail.com>
2026-08-19 16:40:14 +0500

    remove static client serving from server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const path = require('path');
 const dns = require('dns');
 const expressValidator = require('express-validator');
 require('dotenv').config();
@@ -47,15 +46,9 @@ app.use('/api', productRoutes);
 app.use('/api', braintreeRoutes);
 app.use('/api', orderRoutes);
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.get('/', (req, res) => {
+  res.json({ message: 'E-Commerce API is running' });
+});
 
 const PORT = process.env.PORT || 5000;
 
